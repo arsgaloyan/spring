ab53a8731e1b66fe6bfec3c383cbb9c074552275
arsgaloyan <139362615+arsgaloyan@users.noreply.github.com>
2023-08-20 21:06:02 +0300

    Update

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,4 +1,14 @@
 public class Cat {
+    private String message;
+
     public Cat() {
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
